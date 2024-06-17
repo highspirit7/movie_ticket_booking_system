0f3ae974d99c926d01da4882d4912232e0e44ffb
GrownUprince <jake.lee4006@gmail.com>
2024-06-17 14:16:47 +0200

    chore: generate data types

--- src/database/types.ts
+++ src/database/types.ts
@@ -1,35 +1,57 @@
+import type { ColumnType } from "kysely";
+
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>;
+
 export interface Directors {
-  movieId: number
-  personId: number
+  movieId: number;
+  personId: number;
 }
 
 export interface Movies {
-  id: number | null
-  title: string
-  year: number | null
+  id: number | null;
+  title: string;
+  year: number | null;
 }
 
 export interface People {
-  id: number | null
-  name: string
-  birth: number | null
+  birth: number | null;
+  id: number | null;
+  name: string;
 }
 
 export interface Ratings {
-  movieId: number
-  rating: number
-  votes: number
+  movieId: number;
+  rating: number;
+  votes: number;
+}
+
+export interface Screenings {
+  allocatedTickets: number;
+  id: Generated<number>;
+  leftTickets: number;
+  movieId: number;
+  screeningTime: string;
 }
 
 export interface Stars {
-  movieId: number
-  personId: number
+  movieId: number;
+  personId: number;
+}
+
+export interface Tickets {
+  createdAt: Generated<string>;
+  id: Generated<number>;
+  screeningId: number;
 }
 
 export interface DB {
-  directors: Directors
-  movies: Movies
-  people: People
-  ratings: Ratings
-  stars: Stars
+  directors: Directors;
+  movies: Movies;
+  people: People;
+  ratings: Ratings;
+  screenings: Screenings;
+  stars: Stars;
+  tickets: Tickets;
 }
