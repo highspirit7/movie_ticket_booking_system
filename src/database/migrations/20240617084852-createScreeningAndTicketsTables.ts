fc9e142dd3ac591115c6a06f410da4377cc466e8
GrownUprince <jake.lee4006@gmail.com>
2024-06-17 12:23:57 +0200

    feat: add new tables(screenings, tickets)

---
+++ src/database/migrations/20240617084852-createScreeningAndTicketsTables.ts
@@ -0,0 +1,35 @@
+/**
+ * Migration: 20240617084852-createScreeningAndTicketsTables.ts
+ */
+import { Kysely, SqliteDatabase, sql } from 'kysely'
+
+export async function up(db: Kysely<SqliteDatabase>): Promise<void> {
+  await db.schema
+    .createTable('screenings')
+    .ifNotExists()
+    .addColumn('id', 'integer', (c) => c.primaryKey().autoIncrement().notNull())
+    .addColumn('screening_time', 'datetime', (c) => c.notNull())
+    .addColumn('allocated_tickets', 'integer', (c) => c.notNull())
+    .addColumn('left_tickets', 'integer', (c) => c.notNull())
+    .addColumn('movie_id', 'integer', (c) =>
+      c.notNull().references('movies.id')
+    )
+    .execute()
+
+  await db.schema
+    .createTable('tickets')
+    .ifNotExists()
+    .addColumn('id', 'integer', (c) => c.primaryKey().autoIncrement().notNull())
+    .addColumn('screening_id', 'integer', (c) =>
+      c.notNull().references('screenings.id')
+    )
+    .addColumn('created_at', 'text', (col) =>
+      col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull()
+    )
+    .execute()
+}
+
+export async function down(db: Kysely<SqliteDatabase>) {
+  await db.schema.dropTable('screenings').execute()
+  await db.schema.dropTable('tickets').execute()
+}
