e6deb96f1e7dbd56529c3c13f9a21bd0591b6b46
GrownUprince <jake.lee4006@gmail.com>
2024-06-19 15:30:13 +0200

    feat: include movie data as well when finding screenings data

--- src/modules/screenings/repository.ts
+++ src/modules/screenings/repository.ts
@@ -10,18 +10,45 @@ import type { Database, Screening, DB } from '@/database'
 const TABLE = 'screenings'
 type TableName = typeof TABLE
 type Row = Screening
+type RowWithMovie = Row & { movieTitle: string; movieYear: number | null }
 type RowWithoutId = Omit<Row, 'id'>
 type RowInsert = Insertable<RowWithoutId>
 // type RowUpdate = Updateable<RowWithoutId>
 type RowSelect = Selectable<Row>
+type RowSelectWithMovie = Selectable<RowWithMovie>
 
 export default (db: Database) => ({
   findAll: (
     expression?: ExpressionOrFactory<DB, TableName, SqlBool>
-  ): Promise<RowSelect[]> =>
+  ): Promise<RowSelectWithMovie[]> =>
     expression
-      ? db.selectFrom(TABLE).where(expression).select(keys).execute()
-      : db.selectFrom(TABLE).select(keys).execute(),
+      ? db
+          .selectFrom(TABLE)
+          .innerJoin('movies', 'screenings.movieId', 'movies.id')
+          .where(expression)
+          .select([
+            'screenings.id as id',
+            'allocatedTickets',
+            'leftTickets',
+            'screeningTime',
+            'movieId',
+            'movies.title as movieTitle',
+            'movies.year as movieYear',
+          ])
+          .execute()
+      : db
+          .selectFrom(TABLE)
+          .innerJoin('movies', 'screenings.movieId', 'movies.id')
+          .select([
+            'screenings.id as id',
+            'allocatedTickets',
+            'leftTickets',
+            'screeningTime',
+            'movieId',
+            'movies.title as movieTitle',
+            'movies.year as movieYear',
+          ])
+          .execute(),
   create: async (record: RowInsert): Promise<RowSelect | undefined> =>
     db.insertInto(TABLE).values(record).returning(keys).executeTakeFirst(),
 })
